4e21cd6187b9e62be79b07c8d9095fbae84fcfa8
Erick Cifuentes <Erickalfred29@gmail.com>
2020-10-12 23:26:45 -0600

    se agrego un formulario de nuevo y un formulario de edicion

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.publicacion_lista, name='publicacion_lista'),
-     path('publicacion/<int:pk>/', views.publicacion_detalle, name='detalle_publicacion'),
+    path('publicacion/<int:pk>/', views.publicacion_detalle, name='detalle_publicacion'),
+    path('publicacion/nueva', views.publicacion_nueva, name='publicacion_nueva'),
+    path('publicacion/<int:pk>/editar', views.publicacion_editar, name='publicacion_editar'),
 ]
